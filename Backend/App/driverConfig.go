e737e750879e01a98a9d5e4f6f4e10c8e77254fb
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-14 23:34:17 -0300

    fix: driveconfig remove mysql option

--- Backend/App/driverConfig.go
+++ Backend/App/driverConfig.go
@@ -10,17 +10,6 @@ const DbName = "contacts"
 const TableName = "contact"
 
 var Port = "5432"
-var DataSourceName = ""
 
-func init() {
-	if DatabaseDriver == "postgres" {
-		var DataSourceName = fmt.Sprintf("host=%s port=%s user=%s "+
-			"password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
-		fmt.Println(DataSourceName)
-	} else {
-		Port = "3306"
-		var DataSourceName = fmt.Sprintf("%s:%s@%s(:%s)/%s",
-			User, Password, Host, Port, DbName)
-		fmt.Println(DataSourceName)
-	}
-}
+var DataSourceName = fmt.Sprintf("host=%s port=%s user=%s "+
+	"password=%s dbname=%s sslmode=disable", Host, Port, User, Password, DbName)
